3175aece07f2f3e1d4aced33979a0c33b1e03920
tarek-muna <tarek.muna@gmail.com>
2026-08-05 18:02:25 +0200

    release: Version auf 5.0.0 setzen

--- src/NTBPW.PdfWatcher/AppIdentity.cs
+++ src/NTBPW.PdfWatcher/AppIdentity.cs
@@ -3,7 +3,7 @@ namespace NTBPW.PdfWatcher;
 internal static class AppIdentity
 {
     public const string ProductName = "NTB-PW PDF-Watcher Professional";
-    public const string DisplayVersion = "5.0.0-alpha.1";
+    public const string DisplayVersion = "5.0.0";
     public const string Company = "Netz, Technik, Büro GbR";
     public const string Copyright = "© Tarek Muna 2026";
 
